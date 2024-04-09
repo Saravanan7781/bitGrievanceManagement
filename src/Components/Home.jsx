ac1641143bf1dde498b33c468de39a2512ce9993
Saravanan7781 <saravanansomasundharamk@gmial.com>
2024-04-09 15:15:16 +0530

    login-page-half

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import '../Css/Home.css'
-
+import { BsCalendar2Check,BsClipboard2DataFill  } from "react-icons/bs";
+import { MdOutlinePendingActions } from "react-icons/md";
 
 function Home() {
   const list = [{id:1,title:"No.of.Complaints",total:10},{id:2,title:"Complaints Resolved",total:2},
-  {id:3,title:"Complaints pending",total:8},{id:4,title:"No.of.-",total:"-"}]
+  {id:3,title:"Complaints pending",total:8}]
   
   return (
     <>
@@ -12,8 +13,17 @@ function Home() {
           <div className="dashboardGrid">
                 {
                   list.map((ele)=>(
+                    
                     <div className="dashboardInfos" key={ele.id}>
                     <div className="info">
+                      
+                      <div className="infoIcons">
+                      {
+                        ele.id===1?<BsCalendar2Check />: ele.id===2?<BsClipboard2DataFill />:<MdOutlinePendingActions />
+                       
+                      }
+
+                      </div>
                          <h1>{ele.title}</h1>
                           <h1>{ele.total}</h1>
     

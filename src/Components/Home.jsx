b372fe7f045dfba5874902365dd8902b4c5609eb
Saravanan7781 <saravanansomasundharamk@gmial.com>
2024-04-09 16:27:48 +0530

    admin-home-dashboard-notAnimated

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,21 +2,40 @@ import React from 'react'
 import '../Css/Home.css'
 import { BsCalendar2Check,BsClipboard2DataFill  } from "react-icons/bs";
 import { MdOutlinePendingActions } from "react-icons/md";
+import { useState } from 'react';
 
 function Home() {
-  const list = [{id:1,title:"No.of.Complaints",total:10},{id:2,title:"Complaints Resolved",total:2},
-  {id:3,title:"Complaints pending",total:8}]
-  
+  // const list = [{id:1,title:"TOTAL COMPLAINTS",total:10},{id:2,title:" RESOLVED",total:2},
+  // {id:3,title:" PENDING",total:8}]
+  const [count,setCount] = useState(0);
+  const list = [{id:1,title:"TOTAL COMPLAINTS",total:10}]
+  const displayGrid = () =>{
+    setTimeout(()=>{
+      if(count<3){
+      console.log(count);
+      setCount(count+1);
+    }
+
+    else{
+      clearInterval(displayGrid)
+    }
+    }
+      ,1000
+    )
+  }
+
+  displayGrid();
+
   return (
     <>
        <div className='homeMain'>
-          <div className="dashboardGrid">
-                {
+          <div className={(count===2)?`dashboardGrid`:"displaySingle"}>
+                {/* {
                   list.map((ele)=>(
                     
                     <div className="dashboardInfos" key={ele.id}>
                     <div className="info">
-                      
+                    <h1>{ele.title}</h1>
                       <div className="infoIcons">
                       {
                         ele.id===1?<BsCalendar2Check />: ele.id===2?<BsClipboard2DataFill />:<MdOutlinePendingActions />
@@ -24,7 +43,30 @@ function Home() {
                       }
 
                       </div>
-                         <h1>{ele.title}</h1>
+                    
+                          <h1>{ele.total}</h1>
+    
+                    </div>
+                    </div>
+                  ))
+                    
+
+                } */}
+                
+                {
+                  list.map((ele)=>(
+                    
+                    <div className="dashboardInfos" key={ele.id}>
+                    <div className="info">
+                    <h1>{ele.title}</h1>
+                      <div className="infoIcons">
+                      {
+                        ele.id===1?<BsCalendar2Check />: ele.id===2?<BsClipboard2DataFill />:<MdOutlinePendingActions />
+                       
+                      }
+
+                      </div>
+                    
                           <h1>{ele.total}</h1>
     
                     </div>
@@ -33,6 +75,7 @@ function Home() {
                     
 
                 }
+                
           </div>
       </div>
       </>
